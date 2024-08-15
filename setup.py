bf27ce2d5a09a47298a35345839d71eb655f705b
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-08-15 15:24:47 -0500

    builder

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.0"
+DESCRIPTION = "Stenographer.py - a transcription tool for your content."
+LONG_DESCRIPTION = "Stenographer.py is a somewhat automatized tool for videos and audio transcriptions using the power of ollama and whisper"
+AUTHOR = "Alejandro Alzate Sánchez"
+AUTHOR_EMAIL = "alejandro-alzate@github.com"
+DEPENDENCIES = ["whisper", "ollama"]
+
+setup(
+	#God dammit stenographer is taken
+	name = "whisper-stenographer",
+	version = VERSION,
+	author = AUTHOR,
+	author_email = AUTHOR_EMAIL,
+	description = DESCRIPTION,
+	long_description = LONG_DESCRIPTION,
+	packages = find_packages(),
+	install_requires = DEPENDENCIES,
+
+	#Of course nowadays everyone puts this buzzword because,
+	#they of course do so don't mind me if i do it as well.
+	#--------------------------↓
+	keywords = ["automation", "ai", "captioning"],
+	classifiers = [
+		"Development Status :: 3 - Alpha"
+		,"Programming Language :: Python :: 3"
+
+		# The PyPi documentation is a painful mess
+		# So I'm not even bothering, luarocks wins
+		# by on a land slide with it's simplicity.
+
+		#,"Intended Audience :: Accessibility"
+		#,"Operating System :: Linux :: Linux"
+		#,"Operating System :: Microsoft :: Windows"
+		#,"Operating System :: MacOS :: MacOS X"
+	]
+	)
